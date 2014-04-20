39868207bfa187561b90aff9524f01f025b4ae9b
Ricardo Rubio <rickarubio@gmail.com>
2014-04-20 03:43:24 -0700

    done for tonight, left off on figuring out why ocean infinite looping isn't working

--- GamePlay.m
+++ GamePlay.m
@@ -8,7 +8,7 @@
 
 #import "GamePlay.h"
 
-static const CGFloat scrollSpeed = 80.f;
+static const CGFloat scrollSpeed = 90.f;
 
 @implementation GamePlay {
     CCSprite *_playerShip;
@@ -33,12 +33,21 @@ static const CGFloat scrollSpeed = 80.f;
     for (CCNode *ocean in _oceans) {
         CGPoint oceanWorldPosition = [_physicsNode convertToWorldSpace:ocean.position];
         CGPoint oceanScreenPosition = [self convertToNodeSpace:oceanWorldPosition];
-        
+        NSLog(@"testing = %f", oceanScreenPosition.y);
+//        NSLog(@"TESTING");
         if (oceanScreenPosition.y >= (ocean.contentSize.height)) {
-            NSLog(@"changed");
-            ocean.position = ccp(ocean.position.x, -1 * ocean.contentSize.height);
+            NSLog(@"CHANGED");
+            NSLog(@"CHANGED");
+            NSLog(@"CHANGED");
+//            ocean.position = ccp(ocean.position.x, -1 * ocean.contentSize.height);
         }
     }
 }
 
 @end
+
+
+// if the left corner is one complete width off the screen, move it to the right
+//if (groundScreenPosition.x <= (-1 * ground.contentSize.width)) {
+//    ground.position = ccp(ground.position.x + 2 * ground.contentSize.width, ground.position.y);
+//}
