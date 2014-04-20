d459524a5fabd0fc80d1c588f3973335500f18c2
Ricardo Rubio <rickarubio@gmail.com>
2014-04-20 14:15:06 -0700

    ocean now scrolls infinitely

--- GamePlay.m
+++ GamePlay.m
@@ -8,7 +8,7 @@
 
 #import "GamePlay.h"
 
-static const CGFloat scrollSpeed = 90.f;
+static const CGFloat scrollSpeed = 50.f;
 
 @implementation GamePlay {
     CCSprite *_playerShip;
@@ -26,28 +26,17 @@ static const CGFloat scrollSpeed = 90.f;
     // ship moves upwards
     _playerShip.position = ccp(_playerShip.position.x, _playerShip.position.y + delta * scrollSpeed);
     
-    // ocean moves downwards
+    // physicsNode moves downwards
     _physicsNode.position = ccp(_physicsNode.position.x, _physicsNode.position.y - (scrollSpeed * delta));
     
     // loop the ocean background
     for (CCNode *ocean in _oceans) {
         CGPoint oceanWorldPosition = [_physicsNode convertToWorldSpace:ocean.position];
         CGPoint oceanScreenPosition = [self convertToNodeSpace:oceanWorldPosition];
-        NSLog(@"testing = %f", oceanScreenPosition.y);
-//        NSLog(@"TESTING");
-        if (oceanScreenPosition.y >= (ocean.contentSize.height)) {
-            NSLog(@"CHANGED");
-            NSLog(@"CHANGED");
-            NSLog(@"CHANGED");
-//            ocean.position = ccp(ocean.position.x, -1 * ocean.contentSize.height);
+//        NSLog(@"ocean Y-coord = %f", oceanScreenPosition.y);
+        if (oceanScreenPosition.y <= -1 * ocean.contentSize.height) {
+            ocean.position = ccp(0, ocean.position.y + (2 * ocean.contentSize.height));
         }
     }
 }
-
 @end
-
-
-// if the left corner is one complete width off the screen, move it to the right
-//if (groundScreenPosition.x <= (-1 * ground.contentSize.width)) {
-//    ground.position = ccp(ground.position.x + 2 * ground.contentSize.width, ground.position.y);
-//}
